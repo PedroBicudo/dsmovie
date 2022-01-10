c99a720d93347e719edbef10736e0a1820cbd3fe
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-01-10 15:44:35 -0300

    Navigation buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,5 +1,6 @@
 import MovieScore from "components/MovieScore";
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const MovieCard: React.FC = () => {
@@ -17,7 +18,10 @@ const MovieCard: React.FC = () => {
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <MovieScore />
-                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                
+                <Link to={`/form/${movie.id}`}>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                </Link>
             </div>
         </div>
     );
--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 const Form: React.FC = () => {
@@ -47,9 +48,12 @@ const Form: React.FC = () => {
                         </button>
                     </div>
                 </form>
-                <button className="btn btn-primary dsmovie-btn mt-3">
-                    Cancelar
-                </button>
+
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">
+                        Cancelar
+                    </button>
+                </Link>
             </div>
         </div>
     );
