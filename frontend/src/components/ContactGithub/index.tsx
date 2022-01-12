18a6c9cf9a0f2c77b5cc8e1c05c0efedf245ef28
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-01-12 20:52:22 -0300

    add missing noreferrer

--- frontend/src/components/ContactGithub/index.tsx
+++ frontend/src/components/ContactGithub/index.tsx
@@ -5,7 +5,7 @@ import "./styles.css";
 
 const ContactGithub: React.FC = () => {
     return (
-        <a className="dsmovie-contact-github" href="https://github.com/PedroBicudo" target="_blank">
+        <a className="dsmovie-contact-github" href="https://github.com/PedroBicudo" target="_blank" rel="noreferrer">
             <div>
                     <GithubIcon className="dsmovie-contact-github__icon"/>
                     <p className="dsmovie-contact-github__name">/PedroBicudo</p>
