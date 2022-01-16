6a1728329c592633e28be4c277356eed5983cf3d
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-01-16 12:57:27 -0300

    Refactor navbar component

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -4,8 +4,8 @@ import "./styles.css";
 export default function Navbar() {
     return (
       <nav className="container">
-        <div className="dsmovie-nav-content">
-          <h1>DSMovie</h1>
+        <div className="dsmovie-nav">
+          <h1 className="dsmovie-nav__title">DSMovie</h1>
           <ContactGithub />
         </div>
     </nav>
