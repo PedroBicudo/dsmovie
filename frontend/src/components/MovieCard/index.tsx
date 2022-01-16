9c2aeb9a80917022f62bed710507471f8a7ea451
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-01-16 12:33:27 -0300

    Refactor moviecard component

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -2,6 +2,7 @@ import MovieScore from "components/MovieScore";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
+import "./styles.css";
 
 
 type Props = {
@@ -11,14 +12,14 @@ type Props = {
 const MovieCard: React.FC<Props> = ({ movie }) => {
         
     return (
-        <div>
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="dsmovie-card-bottom-container">
-                <h3>{movie.title}</h3>
+        <div className="dsmovie-listing-item">
+            <img className="dsmovie-listing-item__image" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-listing-item__content">
+                <h3 className="dsmovie-listing-item__title">{movie.title}</h3>
                 <MovieScore score={movie.score} totalReviews={movie.totalReviews}/>
                 
                 <Link to={`/form/${movie.id}`}>
-                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                    <div className="btn btn-primary dsmovie-listing-item__btn">Avaliar</div>
                 </Link>
             </div>
         </div>
