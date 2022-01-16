efe2191f35a68b1d44b4178631e39752ede8bd90
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-01-16 12:35:39 -0300

    Replace selector with dsmovie-contact-github__content class

--- frontend/src/components/ContactGithub/index.tsx
+++ frontend/src/components/ContactGithub/index.tsx
@@ -6,7 +6,7 @@ import "./styles.css";
 const ContactGithub: React.FC = () => {
     return (
         <a className="dsmovie-contact-github" href="https://github.com/PedroBicudo" target="_blank" rel="noreferrer">
-            <div>
+            <div className="dsmovie-contact-github__content">
                     <GithubIcon className="dsmovie-contact-github__icon"/>
                     <p className="dsmovie-contact-github__name">/PedroBicudo</p>
             </div>
