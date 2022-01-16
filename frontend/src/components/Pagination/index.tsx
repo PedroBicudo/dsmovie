c4c7981b64d3302c99c53739167476e3d7a9f280
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-01-16 13:01:55 -0300

    Refactor pagination component

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -13,18 +13,18 @@ type Props = {
 const Pagination: React.FC<Props> = ({ moviesPage, onChangePagination }) => {
 
     return (
-        <div className="dsmovie-pagination-container">
-            <div className="dsmovie-pagination-box">
+        <div className="dsmovie-pagination">
+            <div className="dsmovie-pagination__box">
                 <button 
-                    className="dsmovie-pagination-button" 
+                    className="dsmovie-pagination__btn" 
                     disabled={moviesPage.first}
                     onClick={() => onChangePagination(moviesPage.number-1)}
                     >
                     <Arrow />
                 </button>
-                <p>{`${moviesPage.number+1} de ${moviesPage.totalPages}`}</p>
+                <p className="dsmovie-pagination__pages-text">{`${moviesPage.number+1} de ${moviesPage.totalPages}`}</p>
                 <button 
-                    className="dsmovie-pagination-button" 
+                    className="dsmovie-pagination__btn"
                     disabled={moviesPage.last} 
                     onClick={() => onChangePagination(moviesPage.number+1)}
                     >
