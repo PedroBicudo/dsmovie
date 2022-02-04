de160746468b5a1ae7ca7864f27abe92fec3bb23
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-02-04 15:28:55 -0300

    feat(rest): add ExceptionHandler to handle invalid argument type

--- backend/src/main/java/com/devsuperior/dsmovie/rest/advice/MovieControllerAdvice.java
+++ backend/src/main/java/com/devsuperior/dsmovie/rest/advice/MovieControllerAdvice.java
@@ -6,9 +6,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class MovieControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public APIError handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        return new APIError("Invalid argument type");
+    }
 
     @ExceptionHandler(MovieNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
