c78947a6637d09b8bfe46652932a78ea4bdebfa8
Pedro <43938917+PedroBicudo@users.noreply.github.com>
2022-02-07 15:17:59 -0300

    refactor(components): refactor FormCard into smaller components (#18)

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -5,7 +5,7 @@ import { Flex } from "styles/layouts/Flex";
 import { Movie } from "types/movie";
 import { BASE_URL } from "utils/requests";
 import { validateEmail } from "utils/validate";
-import { Card, CardButton, CardContent, CardImage, CardTitle, Form, FormGroup, FormGroupInput, FormGroupLabel, FormGroupSelect } from "./styles";
+import { CardContainer, Button, CardContentContainer, Image, Title, Form, FormGroup, FormGroupInput, FormGroupLabel, FormGroupSelect } from "./styles";
 
 type Props = {
     movieId: string
@@ -50,50 +50,124 @@ const FormCard: React.FC<Props> = ({ movieId }) => {
     }
 
     return (
-        <Card>
-            <CardImage
-                src={movie?.image}
-                alt={movie?.title}
-            />
-            <CardContent>
-                <CardTitle>{movie?.title}</CardTitle>
-                <Form onSubmit={handleSubmit}>
-                    <FormGroup>
-                        <FormGroupLabel htmlFor="email">Informe seu email</FormGroupLabel>
-                        <FormGroupInput
-                            type="email"
-                            className="form-control dsmovie-form__input"
-                            id="email"
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <FormGroupLabel htmlFor="score">Informe sua avaliação</FormGroupLabel>
-                        <FormGroupSelect className="form-control dsmovie-form__input" id="score">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </FormGroupSelect>
-                    </FormGroup>
-                    <Flex gap={0} justify="center">
-                        <CardButton
-                            type="submit"
-                        >
-                            Salvar
-                        </CardButton>
-                    </Flex>
-
-                </Form>
-
-                <Link to="/">
-                    <CardButton>
-                        Cancelar
-                    </CardButton>
-                </Link>
-            </CardContent>
-        </Card>
+        <Card movie={movie} handleSubmitEvent={handleSubmit} />
     );
 };
+
+interface CardProps {
+    movie?: Movie;
+    handleSubmitEvent: Function;
+}
+
+const Card: React.FC<CardProps> = ({movie, handleSubmitEvent}) => {
+    return (
+        <CardContainer>
+            <CardImage image={movie?.image} title={movie?.title} />
+            <CardContent title={movie?.title} handleSubmitEvent={handleSubmitEvent}/>
+        </CardContainer>
+    );
+
+}
+
+interface CardImageProps {
+    image?: string;
+    title?: string;
+}
+
+const CardImage: React.FC<CardImageProps> = ({ image, title }) => {
+    return (
+        <Image
+            src={image}
+            alt={title}
+        />
+
+    );
+
+}
+
+interface CardContentProps {
+    title?: string;
+    handleSubmitEvent: any;
+}
+
+const CardContent: React.FC<CardContentProps> = ({title, handleSubmitEvent}) => {
+    return (
+        <CardContentContainer>
+                <CardTitle title={title} />
+                <Form onSubmit={handleSubmitEvent}>
+                    <EmailFormGroup />
+                    <SelectFormGroup />
+                    <ButtonSubmit />
+                </Form>
+
+                <ButtonCancel/>
+        </CardContentContainer>
+    );
+}
+
+interface CardTitleProps {
+    title?: string
+}
+
+const CardTitle: React.FC<CardTitleProps> = ({ title }) => {
+    return (
+        <Title>{title}</Title>
+    );
+}
+
+const EmailFormGroup: React.FC = () => {
+    return (
+        <FormGroup>
+            <FormGroupLabel htmlFor="email">Informe seu email</FormGroupLabel>
+            <FormGroupInput
+                type="email"
+                className="form-control dsmovie-form__input"
+                id="email"
+            />
+        </FormGroup>
+    );
+    
+}
+
+const SelectFormGroup: React.FC = () => {
+    return (
+        <FormGroup>
+            <FormGroupLabel htmlFor="score">Informe sua avaliação</FormGroupLabel>
+            <FormGroupSelect className="form-control dsmovie-form__input" id="score">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </FormGroupSelect>
+        </FormGroup>
+    );
+}
+
+const ButtonSubmit: React.FC = () => {
+    return (
+        <Flex gap={0} justify="center">
+            <Button
+                type="submit"
+            >
+                Salvar
+            </Button>
+        </Flex>
+
+    );
+}
+
+const ButtonCancel: React.FC = () => {
+    return (
+        <Link to="/">
+            <Button>
+                Cancelar
+            </Button>
+        </Link>
+
+    );
+}
+
+
 
 export default FormCard;
