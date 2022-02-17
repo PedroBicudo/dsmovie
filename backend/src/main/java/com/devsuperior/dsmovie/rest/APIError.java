1dcea9fb3df99a69ee549a48337ecc356be1b988
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-02-17 11:13:36 -0300

    refactor(backend/rest): add ExceptionHandler for validations

--- backend/src/main/java/com/devsuperior/dsmovie/rest/APIError.java
+++ backend/src/main/java/com/devsuperior/dsmovie/rest/APIError.java
@@ -4,9 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class APIError {
-    private String message;
+    private List<String> errors;
+
+    public APIError(String error) {
+        errors = Arrays.asList(error);
+    }
 }
--- backend/src/main/java/com/devsuperior/dsmovie/rest/advice/MovieControllerAdvice.java
+++ backend/src/main/java/com/devsuperior/dsmovie/rest/advice/MovieControllerAdvice.java
@@ -3,10 +3,14 @@ package com.devsuperior.dsmovie.rest.advice;
 import com.devsuperior.dsmovie.model.exceptions.MovieNotFoundException;
 import com.devsuperior.dsmovie.rest.APIError;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class MovieControllerAdvice {
@@ -21,5 +25,18 @@ public class MovieControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public APIError handleMovieNotFoundException(MovieNotFoundException e) {
         return new APIError(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public APIError handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<String> errors = e
+                .getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return new APIError(errors);
     }
 }
