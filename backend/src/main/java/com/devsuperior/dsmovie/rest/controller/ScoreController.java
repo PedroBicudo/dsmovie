5cd6e0447bec1cf7db13d9ba3a30a72b0cac98aa
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-04-25 13:09:35 -0300

    docs(backend): add dsmovie API documentation

--- backend/src/main/java/com/devsuperior/dsmovie/rest/controller/ScoreController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/rest/controller/ScoreController.java
@@ -3,6 +3,7 @@ package com.devsuperior.dsmovie.rest.controller;
 import com.devsuperior.dsmovie.rest.dto.MovieDTO;
 import com.devsuperior.dsmovie.rest.dto.ScoreDTO;
 import com.devsuperior.dsmovie.model.services.ScoreService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,13 +14,21 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/scores")
+@Api
 public class ScoreController {
 
     @Autowired
     private ScoreService service;
 
+    @ApiOperation("Add or update a score")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Movie not found"),
+            @ApiResponse(code = 200, message = "Score successfully registered")
+    })
     @PutMapping
-    public MovieDTO saveScore(@RequestBody @Valid ScoreDTO scoreDTO) {
+    public MovieDTO saveScore(
+            @ApiParam("User's score") @RequestBody @Valid ScoreDTO scoreDTO
+    ) {
         MovieDTO movieDTO = service.saveScore(scoreDTO);
 
         return movieDTO;
